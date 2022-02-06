0cae83ec5b988a5c02addc0f03693009ee80be0c
Max <maks-keznecov11@mail.ru>
2022-02-06 16:23:15 +0300

    js part_2 practic

--- JS/script.js
+++ JS/script.js
@@ -11,12 +11,39 @@ const personalMoviDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
+if (personalMoviDB.count < 10){
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMoviDB.count < 30){
+    alert("Вы классический зритель");
+} else if (personalMoviDB.count > 30){
+    alert("Вы киноман");
+} else{
+   alert("Произошла ошибка");
+}
 
-personalMoviDB.movies[a] = b;
-personalMoviDB.movies[c] = d;
+// for (let i = 0; i<2; i++){
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//           b = prompt("На сколько оцените его?", "");
+//           personalMoviDB.movies[a] = b;
 
-console.log(personalMoviDB);
+//           if (a != null && b != null && a != "" && b != "" && a.length <50 && b.length <50){
+//              personalMoviDB.movies[a] = b;
+//           } else {
+//               i = 0;
+//           }
+// }
+
+let num = 0;
+
+while (num < 2){
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+          personalMoviDB.movies[a] = b;
+
+          if (a != null && b != null && a != "" && b != "" && a.length <50 && b.length <50){
+             personalMoviDB.movies[a] = b;
+             num++;
+          } else {
+              num = 0;
+          }
+}
